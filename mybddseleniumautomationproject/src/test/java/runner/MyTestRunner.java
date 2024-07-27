29a62795d77c91db3b44a05d05103753b9021291
felicityautomation <115887420+felicityautomation@users.noreply.github.com>
2024-07-27 09:37:41 +0530

    Update MyTestRunner.java
    Added commits

--- mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
+++ mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
@@ -2,7 +2,8 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//Testing
+//Done by GIT
 @CucumberOptions(features = {""},glue = {""},plugin = {"test345"})
 public class MyTestRunner extends AbstractTestNGCucumberTests {
 
