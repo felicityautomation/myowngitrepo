66e01a3b5b5525cc969f7add30a56a7e04f84c14
felicityautomation <115887420+felicityautomation@users.noreply.github.com>
2024-07-27 09:36:06 +0530

    Update MyTestRunner.java

--- mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
+++ mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {""},glue = {""},plugin = {"test"})
+@CucumberOptions(features = {""},glue = {""},plugin = {"test345"})
 public class MyTestRunner extends AbstractTestNGCucumberTests {
 
 }
