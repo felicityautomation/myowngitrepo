aa0f3a7d3c0bc5a7cbe761ae942c0ec8d1baae47
Bharath <you@example.com>
2024-07-27 09:34:34 +0530

    Modified runner files

--- mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
+++ mybddseleniumautomationproject/src/test/java/runner/MyTestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {""},glue = {""},plugin = {""})
+@CucumberOptions(features = {""},glue = {""},plugin = {"test"})
 public class MyTestRunner extends AbstractTestNGCucumberTests {
 
 }
